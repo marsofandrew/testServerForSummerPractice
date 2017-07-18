4069509db0f524a871b0e0bc9cf8c5c1c2f260b5
marsofandrew <marsofandrew@mail.ru>
2017-07-18 15:10:25 +0300

    make code clearly

--- src/main/java/spbspu/icc/ics/Main.java
+++ src/main/java/spbspu/icc/ics/Main.java
@@ -1,31 +1,10 @@
 package spbspu.icc.ics;
-/**
- * Created by Андрей on 11.07.2017.
- */
-import org.zeromq.ZContext;
-import org.zeromq.ZFrame;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.PollItem;
-import org.zeromq.ZMQ.Socket;
-import org.zeromq.ZMsg;
-import org.zeromq.ZMQ.Poller;
-
-import java.io.IOException;
-import java.nio.channels.Selector;
-import java.util.Random;
-
-//
-//Asynchronous client-to-server (DEALER to ROUTER)
-//
-//While this example runs in a single process, that is just to make
-//it easier to start and stop the example. Each task has its own
-//context and conceptually acts as a separate process.
 
 public class Main
 {
 
     public static void main(String[] args) throws Exception {
-        ZContext ctx = new ZContext();
+
         new Thread(new ClientTask("0D05-115C")).start();
         new Thread(new ClientTask("08AE-0D05")).start();
         new Thread(new ClientTask("115C-15B3")).start();
@@ -33,6 +12,5 @@ public class Main
 
         //  Run for 5 seconds then quit
         Thread.sleep(5 * 1000);
-        ctx.destroy();
     }
 }
